3ad200fbceaa4015c8b489f3d1a13cf53c9585fb
brian ambundo <brian.katiechi9@gmail.com>
2022-06-07 13:01:38 +0300

    commit tree 1f05421a0bc8bea26ec8a97cb02a24d5305ec397

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "Brian Ambundo"
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+console.log(excitedWelcomeMessage)
+
+const splitString = currentUser.split(' ')
+console.log(splitString)
+const shortGreeting = `Welcome, ${splitString[0]} !`
